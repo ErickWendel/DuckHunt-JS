7e1ff3818f9257516c4ecc2770c40d250ae0e6d8
WellsSA <wel.cavzod@gmail.com>
2022-11-21 13:27:27 -0300

    chore(public/animation): removing comments

--- mobile-controller/public/animation.js
+++ mobile-controller/public/animation.js
@@ -8,28 +8,15 @@ const animationMap = {
   '#dot': 'cool 2s',
   '#dot1': 'cooll 2s',
   '#dot2': 'coolll 2s',
-  // '#bullet': 'bang 1.1s',
-  // '#bullet1': 'bangg 1.1s',
-  // '#bullet2': 'banggg 1.1s',
-  // '#bullet3': 'bangggg 1.1s',
-  // '#bullet4': 'banggg2 1.1s',
-  // '#bullet5': 'banggg3 1.1s',
-  // '#bullet6': 'banggg4 1.1s',
-  // '#bullet7': 'banggg5 1.1s',
-  // '#bullet8': 'banggg6 1.1s',
 };
 
 const animationMapKeys = Object.keys(animationMap);
 
 const shoot = () => {
-  const shotID = new Date().getTime();
-  const customSelector = `#shot-${shotID}`;
   const base = $('.bullets');
-
   const newBullets = base.cloneNode(true);
 
   newBullets.style.opacity = '1';
-  newBullets.classList.add(customSelector);
 
   base.parentNode.appendChild(newBullets);
 
